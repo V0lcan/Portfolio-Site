e55aaf56b49c98d6b2752b154f06b5c27dbec5ee
V0lcan <albinsand@hotmail.se>
2023-03-04 12:48:39 +0100

    Changed comments

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,20 @@
+// NIGHTMODE SWITCH
+// Setting standard color values for the css variables
 document.documentElement.style.setProperty('--light-color', '#FFF');
 document.documentElement.style.setProperty('--dark-color', '#282828');
 document.documentElement.style.setProperty('--header-bg-color', '#f3f3f3');
 document.documentElement.style.setProperty('--header-bg-text-color', '#f0f0f0');
 
-
-
 // Get a reference to the checkbox with the ID "nightswitch"
 const nightSwitch = document.getElementById('nightswitch');
 
-// Add an event listener to the checkbox that listens for changes in its state
+// Event listener to the checkbox that listens for changes in its state
 nightSwitch.addEventListener('change', function() {
 
-  // Check if the checkbox is checked
+  // Is the checkbox checked?
   if (this.checked) {
 
-    // If it is checked, set the values of the --primary-color and --secondary-color CSS variables to new values
+    // If it is checked, set the values of the CSS variables to these values
     document.documentElement.style.setProperty('--light-color', '#FFF');
     document.documentElement.style.setProperty('--dark-color', '#282828');
     document.documentElement.style.setProperty('--header-bg-color', '#f3f3f3');
@@ -22,7 +22,7 @@ nightSwitch.addEventListener('change', function() {
 
   } else {
 
-    // If it's not checked, set the values of the --primary-color and --secondary-color CSS variables back to the original values
+    // If it's not checked, set the values of the CSS variables to these values
     document.documentElement.style.setProperty('--light-color', '#282828');
     document.documentElement.style.setProperty('--dark-color', '#FFF');
     document.documentElement.style.setProperty('--header-bg-color', '#252525');
